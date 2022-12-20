17a091a16e62e59a6ad4f207837d8066e5501d3a
Caner AY <caneray34@hotmail.com>
2022-12-20 11:31:36 +0300

    Repository eklendi

---
+++ CarDetails.BL/Repository/IRepository/GenericRepository.cs
@@ -0,0 +1,43 @@
+﻿using CarDetails.DL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDetails.BL.Repository.IRepository
+{
+    public class GenericRepository<T> where T : class, new()
+    {
+        Context c = new Context();
+        public List<T> TList()
+        {
+            return c.Set<T>().ToList();
+        }
+        public void TAdd(T t)
+        {
+            c.Set<T>().Add(t);
+            c.SaveChanges();
+        }
+        public void TDelete(T t)
+        {
+            c.Set<T>().Remove(t);
+            c.SaveChanges();
+        }
+        public void TUpdate(T t)
+        {
+            c.Set<T>().Update(t);
+            c.SaveChanges();
+        }
+        public T TGet(int id)
+        {
+            return c.Set<T>().Find(id);
+        }
+
+        public List<T> TList(string p)
+        {
+            return c.Set<T>().Include(p).ToList();
+        }
+    }
+}
